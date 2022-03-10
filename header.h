3383b15c904276ccdd41d1ffc1fff2af5194be72
RoyalMike <bibo200311@gmail.com>
2022-03-10 08:37:12 +0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,34 @@
+#include <string>
+#pragma once
+
+struct STAFF
+{
+    string staffusr, staffpwd, firstname, lastname, dob, gender;
+    STAFF* next;
+};
+
+struct TEACHER
+{
+    string teachusr, teachpwd, firstname, lastname, dob, gender;
+    TEACHER* next;
+};
+
+struct STUDENT
+{
+    string studentusr, studentpwd, studentID, firstname, lastname, dob, gender, socialID, startyear, classID, CoursesID;
+    STUDENT* next;
+};
+
+struct LOGIN
+{
+    STAFF* staff;
+    TEACHER* teacher;
+    STUDENT* student;
+    string username, password;
+    int identity = 0;
+};
+
+void loginCheck(LOGIN &data);
+void loginMenu(LOGIN &data);
+void viewInfo(LOGIN data);
+void logOut(LOGIN &data);
