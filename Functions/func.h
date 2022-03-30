17221b484f49e355590c40f8d82462af21d88003
KHOA NGUYỄN ANH <21127517@student.hcmus.edu.vn>
2022-03-30 10:59:16 +0700

    Updating main

---
+++ Functions/func.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <direct.h>
+
+bool isDigit_w(std::string);
+std::string toLower_w(std::string);
+std::string toUpper_w(std::string);
+int countElement(std::string, char);
+bool isValidDate(std::string, int);
