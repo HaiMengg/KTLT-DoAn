5c7d08f2cfb0e55efb799f5895a7d98c3b9b02c6
KHOA NGUYỄN ANH <21127517@student.hcmus.edu.vn>
2022-03-30 09:59:56 +0700

    Updating main

---
+++ Login-Logout/login-logout.cpp
@@ -0,0 +1,254 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+#include "read-write-csv.cpp"
+
+// Login
+void loginCheck(Login &data)
+{
+    if (data.identity == 0) std::cout << "----------------\n" << "Welcome!\nPlease login to continue.\n\n";
+    std::cout << "[1] Login as staff\n[2] Login as teacher\n[3] Login as student\n[4] Exit program\n"
+    << "----------------\n";
+
+    std::cout << "Input: ";
+    std::string option;
+    std::cin >> option;
+
+    if (option == "1" || option == "2" || option == "3")
+    {
+        std::cout << "Username: ";
+        std::cin >> data.username;
+
+        std::cout << "Password: ";
+        std::cin >> data.password;
+
+        if (option == "1")
+        {
+            Staff* cur = data.staff;
+            while (cur != nullptr)
+            {
+                if (data.username == cur -> usr && data.password == cur -> pwd)
+                {
+                    data.identity = 1;
+                    data.curStaff = cur;
+                    break;
+                }
+                cur = cur -> nodeNext;
+            }
+        }
+
+        else if (option == "2")
+        {
+            Teacher* cur = data.teacher;
+            while (cur != nullptr)
+            {
+                if (data.username == cur -> usr && data.password == cur -> pwd)
+                {
+                    data.identity = 2;
+                    data.curTeacher = cur;
+                    break;
+                }
+                cur = cur -> nodeNext;
+            }
+        }
+
+        else
+        {
+            Student* cur = data.student;
+            while (cur != nullptr)
+            {
+                if (data.username == cur -> usr && data.password == cur -> pwd)
+                {
+                    data.identity = 3;
+                    data.curStudent = cur;
+                    break;
+                }
+                cur = cur -> nodeNext;
+            }
+        }
+
+        if (data.identity == 3)
+        {
+            std::cout << "----------------\n"
+            "Welcome " << data.username << "!\n\n"
+            << "[1] View info\n[2] View my courses\n[3] Enroll in a course\n[4] Remove a course\n"
+            << "[5] Change password\n[6] Log out\n[7] Exit program\n"
+            << "----------------\n";
+            studentMenu(data);
+        }
+
+        else if (data.identity > 0)
+        {
+            std::cout << "----------------\n"
+            "Welcome " << data.username << "!\n\n"
+            << "[1] View info\n[2] Change password\n[3] Log out\n[4] Exit program\n"
+            << "----------------\n";
+            loginMenu(data);
+        }
+
+        else
+        {
+            std::cout << "Incorrect username or password. Please try again.\n\n";
+            data.identity = -1;
+            return loginCheck(data);
+        }
+    }
+
+    else if (option == "4") return;
+
+    else
+    {
+        std::cout << "Invalid input. Please try again.\n\n";
+        data.identity = -1;
+        return loginCheck(data);
+    }
+}
+
+// Basic menu
+void loginMenu(Login &data)
+{
+    std::cout << "Input: ";
+    std::string option;
+    std::cin >> option;
+
+    if (option == "1") viewInfo(data);
+    else if (option == "2") changePassword(data);
+    else if (option == "3") logOut(data);
+    else if (option == "4") return;
+    else
+    {
+        std::cout << "Invalid input. Please try again.\n\n";
+        return loginMenu(data);
+    }
+}
+
+// Student menu
+void studentMenu(Login &data)
+{
+    std::cout << "Input: ";
+    std::string option;
+    std::cin >> option;
+
+    if (option == "1") viewInfo(data);
+    else if (option == "2") viewCourse(data);
+    else if (option == "3")
+    {
+        showCourses(data.course);
+        enrollCourse(data);
+    }
+    else if (option == "4") removeCourse(data);
+    else if (option == "5") changePassword(data);
+    else if (option == "6") logOut(data);
+    else if (option == "7") return;
+    else
+    {
+        std::cout << "Invalid input. Please try again.\n";
+        return studentMenu(data);
+    }
+}
+
+// View info
+void viewInfo(Login data)
+{
+    if (data.identity == 1)
+    {
+        Staff* cur = data.curStaff;
+        std::cout << "----------------\n";
+        std::cout << "Username: " << cur -> usr << std::endl;
+        std::cout << "First Name: " << cur -> firstName << std::endl;
+        std::cout << "Last Name: " << cur -> lastName << std::endl;
+        std::cout << "Date of Birth: " << cur -> dob << std::endl;
+        std::cout << "Gender: " << cur -> gender << std::endl;
+        std::cout << "----------------\n";
+        return loginMenu(data);
+    }
+
+    else if (data.identity == 2)
+    {
+        Teacher* cur = data.curTeacher;
+        std::cout << "----------------\n";
+        std::cout << "Username: " << cur -> usr << std::endl;
+        std::cout << "First Name: " << cur -> firstName << std::endl;
+        std::cout << "Last Name: " << cur -> lastName << std::endl;
+        std::cout << "Date of Birth: " << cur -> dob << std::endl;
+        std::cout << "Gender: " << cur -> gender << std::endl;
+        std::cout << "----------------\n";
+        return loginMenu(data);
+    }
+
+    else
+    {
+        Student* cur = data.curStudent;
+        std::cout << "----------------\n";
+        std::cout << "Username: " << cur -> usr << std::endl;
+        std::cout << "Student ID: " << cur -> studentID << std::endl;
+        std::cout << "First Name: " << cur -> firstName << std::endl;
+        std::cout << "Last Name: " << cur -> lastName << std::endl;
+        std::cout << "Date of Birth: " << cur -> dob << std::endl;
+        std::cout << "Gender: " << cur -> gender << std::endl;
+        std::cout << "Social ID: " << cur -> socialID << std::endl;
+        std::cout << "Start Year: " << cur -> startYear << std::endl;
+        std::cout << "Class ID: " << cur -> classID << std::endl;
+        std::cout << "----------------\n";
+        return studentMenu(data);
+    }
+}
+
+// Change password
+void changePassword(Login &data)
+{
+    std::string pass;
+    std::cout << "Input current password: ";
+    std::cin >> pass;
+
+    while (pass != data.password)
+    {
+        std::cout << "Incorrect password. Try again: ";
+        std::cin >> pass;
+    }
+
+    std::cout << "Input new password: ";
+    std::cin >> pass;
+
+    if (data.identity == 1)
+    {
+        Staff* cur = data.curStaff;
+        cur -> pwd = pass;
+        writeStaff(data.staff);
+    }
+
+    else if (data.identity == 2)
+    {
+        Teacher* cur = data.curTeacher;
+        cur -> pwd = pass;
+        writeTeacher(data.teacher);
+    }
+
+    else
+    {
+        Student* cur = data.curStudent;
+        cur -> pwd = pass;
+        writeStudent(data.student);
+    }
+
+    data.password = pass;
+    std::cout << "Password changed successfully!\n" << "----------------\n";
+
+    if (data.identity == 3) studentMenu(data);
+    else loginMenu(data);
+}
+
+// Log out
+void logOut(Login &data)
+{
+    data.username = "";
+    data.password = "";
+    data.identity = 0;
+    data.curStaff = nullptr;
+    data.curTeacher = nullptr;
+    data.curStudent = nullptr;
+    loginCheck(data);
+}
---
+++ Login-Logout/read-write-csv.cpp
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+// Read student's courses
+void readCourse(std::string courses, std::string schoolYear, StudentCourse* &head)
+{
+    int count = 3;
+    StudentCourse* headCourse = new StudentCourse;
+    StudentCourse* curCourse = headCourse;
+
+    std::istringstream iss(courses);
+    std::string item;
+    while (getline(iss, item, '|'))
+    {
+        if (count % 3 == 0)
+        {
+            if (count != 3)
+            {
+                curCourse -> nodeNext = new StudentCourse;
+                curCourse -> nodeNext -> nodePrev = curCourse;
+                curCourse = curCourse -> nodeNext;
+            }
+
+            curCourse -> schoolYear = atoi(schoolYear.c_str());
+            curCourse -> sem1Courses = item;
+        }
+
+        if (count % 3 == 1)
+        curCourse -> sem2Courses = item;
+
+        if (count % 3 == 2)
+        curCourse -> sem3Courses = item;
+
+        count++;
+    }
+
+    curCourse -> nodeNext = nullptr;
+    head = headCourse;
+}
+
+// Read staff.csv
+void readStaff(Staff* &data, std::fstream &input)
+{
+    // Track current pointer
+    Staff* cur = data;
+
+    // Ignore first line
+    std::string str;
+    std::getline(input, str);
+
+    // Get data
+    while (true)
+    {
+        getline(input, cur -> usr, ',');
+        getline(input, cur -> pwd, ',');
+        getline(input, cur -> firstName, ',');
+        getline(input, cur -> lastName, ',');
+        getline(input, cur -> dob, ',');
+        getline(input, cur -> gender);
+
+        if (input.eof())
+        {
+            cur -> nodeNext = nullptr;
+            break;
+        }
+
+        cur -> nodeNext = new Staff;
+        cur -> nodeNext -> nodePrev = cur;
+        cur = cur -> nodeNext;   
+    }
+    cur = nullptr;
+}
+
+// Read teacher.csv
+void readTeacher(Teacher* &data, std::fstream &input)
+{
+    // Track current pointer
+    Teacher* cur = data;
+
+    // Ignore first line
+    std::string str;
+    std::getline(input, str);
+
+    // Get data
+    while (true)
+    {
+        getline(input, cur -> usr, ',');
+        getline(input, cur -> pwd, ',');
+        getline(input, cur -> firstName, ',');
+        getline(input, cur -> lastName, ',');
+        getline(input, cur -> dob, ',');
+        getline(input, cur -> gender);
+
+        if (input.eof())
+        {
+            cur -> nodeNext = nullptr;
+            break;
+        }
+
+        cur -> nodeNext = new Teacher;
+        cur -> nodeNext -> nodePrev = cur;
+        cur = cur -> nodeNext;   
+    }
+    cur = nullptr;
+}
+
+// Read student.csv
+void readStudent(Student* &data, std::fstream &input)
+{
+    // Track current pointer
+    Student* cur = data;
+
+    // Ignore first line
+    std::string str;
+    std::getline(input, str);
+
+    // Get data from student.csv
+    while (true)
+    {
+        getline(input, cur -> usr, ',');
+        getline(input, cur -> pwd, ',');
+        getline(input, cur -> studentID, ',');
+        getline(input, cur -> firstName, ',');
+        getline(input, cur -> lastName, ',');
+        getline(input, cur -> dob, ',');
+        getline(input, cur -> gender, ',');
+        getline(input, cur -> socialID, ',');
+        getline(input, cur -> startYear, ',');
+        getline(input, cur -> classID, ',');
+
+        std::string schoolYear = cur -> startYear;
+        std::string rawCourses;
+        getline(input, rawCourses);
+        readCourse(rawCourses, schoolYear, cur -> studentCourseHead);
+
+        if (input.eof())
+        {
+            cur -> nodeNext = nullptr;
+            break;
+        }
+
+        cur -> nodeNext = new Student;
+        cur -> nodeNext -> nodePrev = cur;
+        cur = cur -> nodeNext;
+    }
+    cur = nullptr;
+}
+
+// Write to staff.csv
+void writeStaff(Staff* data)
+{
+    std::fstream output;
+    output.open("staff.csv", std::ios::out);
+    output << "usr,pwd,firstName,lastName,dob,gender\n";
+
+    Staff* cur = data;
+    while (cur != nullptr)
+    {
+        output << cur -> usr << ","
+        << cur -> pwd << ","
+        << cur -> firstName << ","
+        << cur -> lastName << ","
+        << cur -> dob << ","
+        << cur -> gender;
+
+        if (cur -> nodeNext != nullptr)
+        output << std::endl;
+
+        cur = cur -> nodeNext;
+    }
+}
+
+// Write to teacher.csv
+void writeTeacher(Teacher* data)
+{
+    std::fstream output;
+    output.open("teacher.csv", std::ios::out);
+    output << "usr,pwd,firstName,lastName,dob,gender\n";
+
+    Teacher* cur = data;
+    while (cur != nullptr)
+    {
+        output << cur -> usr << ","
+        << cur -> pwd << ","
+        << cur -> firstName << ","
+        << cur -> lastName << ","
+        << cur -> dob << ","
+        << cur -> gender;
+
+        if (cur -> nodeNext != nullptr)
+        output << std::endl;
+
+        cur = cur -> nodeNext;
+    }
+}
+
+// Write to student.csv
+void writeStudent(Student* data)
+{
+    std::fstream output;
+    output.open("student.csv", std::ios::out);
+    output << "usr,pwd,studentID,firstName,lastName,dob,gender,socialID,startYear,classID,coursesID\n";
+
+    Student* cur = data;
+    while (cur != nullptr)
+    {
+        output << cur -> usr << ","
+        << cur -> pwd << ","
+        << cur -> studentID << ","
+        << cur -> firstName << ","
+        << cur -> lastName << ","
+        << cur -> dob << ","
+        << cur -> gender << ","
+        << cur -> socialID << ","
+        << cur -> startYear << ","
+        << cur -> classID << ",";
+
+        StudentCourse* curCourse = cur -> studentCourseHead;
+        while (curCourse != nullptr)
+        {
+            if (curCourse -> sem1Courses[0] == '-')
+            curCourse -> sem1Courses.erase(0, 1);
+
+            if (curCourse -> sem2Courses[0] == '-')
+            curCourse -> sem2Courses.erase(0, 1);
+
+            if (curCourse -> sem3Courses[0] == '-')
+            curCourse -> sem3Courses.erase(0, 1);
+
+            if (curCourse != cur -> studentCourseHead)
+            output << "|";
+
+            output << curCourse -> sem1Courses << "|"
+            << curCourse -> sem2Courses << "|"
+            << curCourse -> sem3Courses;
+
+            curCourse = curCourse -> nodeNext;
+        }
+
+        if (cur -> nodeNext != nullptr)
+        output << std::endl;
+
+        cur = cur -> nodeNext;
+    }
+}
+
+// Delete data
+void deleteData(Login &data)
+{
+    Staff *fCur = data.staff, *fDel;
+    Teacher *rCur = data.teacher, *rDel;
+    Student *tCur = data.student, *tDel;
+    Course *cCur = data.course, *cDel;
+
+    while (fCur != nullptr)
+    {
+        fDel = fCur -> nodeNext;
+        delete fCur;
+        fCur = fDel;
+    }
+
+    while (rCur != nullptr)
+    {
+        rDel = rCur -> nodeNext;
+        delete rCur;
+        rCur = rDel;
+    }
+
+    while (tCur != nullptr)
+    {
+        tDel = tCur -> nodeNext;
+        delete tCur;
+        tCur = tDel;
+    }
+
+    while (cCur != nullptr)
+    {
+        cDel = cCur -> nodeNext;
+        delete cCur;
+        cCur = cDel;
+    }
+
+    data.staff = nullptr;
+    data.teacher = nullptr;
+    data.staff = nullptr;
+    data.course = nullptr;
+}
