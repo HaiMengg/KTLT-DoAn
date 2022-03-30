cd46ef845bf89268255a0f2090a74eacde60a6e7
KHOA NGUYỄN ANH <21127517@student.hcmus.edu.vn>
2022-03-30 09:28:55 +0700

    Updating

--- Main-Menu/menu.h
+++ Main-Menu/menu.h
@@ -7,10 +7,16 @@
 #include "../Linked-List/linkedlist.h"
 #include "../Create-Classes/create.h"
 #include "../Create-School-Year/create.h"
+#include "../Create-Semester/create.h"
 #include "../Functions/func.h"
 
-void currentMenu(Node&, std::fstream&, std::fstream&, std::fstream&);
+void currentMenu(Node&, std::fstream&, std::fstream&, std::fstream&, std::fstream&);
 
 void viewMenu(Node allData);
+void createSchoolYearMenu(SchoolYear*& schoolYearHead, std::fstream& schoolYearData);
+
 void createClassMenu(Classes*& classHead, std::fstream& classData, SchoolYear* schoolYearHead);
-void addStudentMenu(Student* studentHead, std::fstream& studentData, SchoolYear* schoolYearHead, Classes* classesHead);
+void addStudentMenu(Student*& studentHead, std::fstream& studentData, SchoolYear* schoolYearHead, Classes*& classesHead);
+
+void createSemesterMenu(Semesters*& semesterHead, std::fstream& semesterData, SchoolYear* schoolYearHead);
+void addCourseMenu(Semesters*& semesterHead, SchoolYear* schoolYearHead);
